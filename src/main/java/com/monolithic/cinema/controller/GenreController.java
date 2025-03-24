80fca6a8266bb58aa735f797f847db027ed454fc
sp1deydev <duckhanhthientran@gmail.com>
2025-03-24 14:13:09 +0700

    fix versoning api

--- src/main/java/com/monolithic/cinema/controller/GenreController.java
+++ src/main/java/com/monolithic/cinema/controller/GenreController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/genres")
+@RequestMapping("/v1/genres")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class GenreController {
--- src/main/java/com/monolithic/cinema/controller/MovieController.java
+++ src/main/java/com/monolithic/cinema/controller/MovieController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/v1/movies")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class MovieController {
